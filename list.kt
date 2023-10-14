3f74d01e287893557282b0450b5652c4dfe9dc29
Clinton Ogada <clintonogada@gmail.com>
2023-10-14 14:39:46 +0300

    Creating  a read-only list

---
+++ list.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // Create a read-only list of fruits containing "Apple," "Banana," and "Cherry."
+    val fruits = listOf("Apple", "Banana", "Cherry")
+
+    // Iterate through each element in the 'fruits' list using a for loop.
+    for (fruit in fruits) {
+        // For each 'fruit' in the list, print a message expressing that you like the specific fruit.
+        println("I like $fruit")
+    }
+}
